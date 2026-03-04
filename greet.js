9262f817b3ddb8e6055f2d291501c50f8204334a
Muhammad Naufal Fadhilah <117992655+nopalllfd@users.noreply.github.com>
2026-03-04 13:18:16 +0700

    feat: added debugging console on params in greet func

--- greet.js
+++ greet.js
@@ -1,4 +1,5 @@
 export const greet = (fullName, random, arr) => {
+  console.log(`${fullName}, ${random}, ${arr}`)  
   const to_greet = arr.map((item, index) => {
     if (index === random) {
       return (place += item);
