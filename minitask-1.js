60f53e27c85c734d3d4db15dbb7ededb1d70b474
nopalllfd <Naufalfadhilah689@gmail.com>
2026-03-04 11:10:58 +0700

    refactor: deleting unused variable

--- minitask-1.js
+++ minitask-1.js
@@ -2,10 +2,8 @@ import moment from 'moment';
 
 const dateConvertion = (date) => {
   const validate = moment(date, 'DD-MM-YYYY', true).isValid();
-  console.log(`tanggal dimasukkan ${date}`);
   if (!validate) {
     throw new Error('Format tanggal salah');
-    console.log(kawidjijawd);
   } else {
     const res = moment(date, 'DD/MM/YYYY');
     const convertedDate = res.format('DD/MM/YYYY');
