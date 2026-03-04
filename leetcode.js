2f5ab569d38afaa0d79af58f756056bb1a485787
nopalllfd <Naufalfadhilah689@gmail.com>
2026-03-04 10:55:18 +0700

    woaaahhhh

---
+++ leetcode.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var minOperations = function (nums, k) {
+  let num = 0;
+  for (let i = 0; i < nums.length; i++) {
+    num += nums[i];
+  }
+  for (let j = 0; j <= k; j++) {
+    console.log(num);
+    if (num < k) {
+      return num;
+    } else {
+      num -= k;
+    }
+  }
+  return num;
+};
+console.log(minOperations([17, 14], 5));
